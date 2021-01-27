5ddd94e67dbbdc99878ea4ed40d9c41dff7a4779
rukidding918 <rukidding918@gmail.com>
2021-01-27 23:19:15 +0900

    init

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book Genre.(e.g. Science Fiction)')
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_legnth=200)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_legnth=1000, help_text='Enter a brief description of book.')
+    isbn = models.CharField('ISBN', max_legnth=13, help_text='13 charactor <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book.')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
--- python_web_book/polls/models.py
+++ python_web_book/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200, help_text='Write the question.')
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
