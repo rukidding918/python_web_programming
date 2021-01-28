96cf0e14730a25d2931868775cede52fe90eb935
rukidding918 <rukidding918@gmail.com>
2021-01-28 19:25:01 +0900

    'init'

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'date_of_birth', 'date_of_death']
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'display_genre']
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ['status', 'due_back']
+    list_display = ['book', 'status']
+
+    fieldsets = (
+        (None, {'fields': ('book', 'imprint', 'id')}),
+        ('Availability', {'fields': ('status', 'due_back')})
+    )
+
+# admin.site.register(Book)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,7 +1,20 @@
+import uuid
 from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
 class Genre(models.Model):
     name = models.CharField(max_length=200, help_text='Enter a book Genre.(e.g. Science Fiction)')
 
@@ -9,10 +22,10 @@ class Genre(models.Model):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_legnth=200)
+    title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_legnth=1000, help_text='Enter a brief description of book.')
-    isbn = models.CharField('ISBN', max_legnth=13, help_text='13 charactor <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of book.')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 charactor <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book.')
 
     def __str__(self):
@@ -20,3 +33,30 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library.')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book Availability')
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
